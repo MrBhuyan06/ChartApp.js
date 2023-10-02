76b512d29bcb4bfc5537a00cac01470372b2486d
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-10-02 19:47:38 +0530

    added the clientjs file

--- js/client.js
+++ js/client.js
@@ -6,16 +6,28 @@ const form = document.getElementById("send-container");
 const msgInp = document.getElementById("messageInp");
 const msgCon = document.querySelector(".container");
 console.log("sasa", msgCon);
-const append = (message, postion) => {
+let appendFun = (message) => {
   const msgEle = document.createElement("div");
   msgEle.innerText = message;
+  console.log(message);
+  console.log(msgEle);
   // msgEle.classList.add('.m')sdwdw
   msgCon.append(msgEle);
 };
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const msg = msgInp.value;
+  appendFun(`you :${msg}`);
+  socket.emit("send", msg);
+  msgInp.value = "";
+});
 const name = prompt("Enter your name to join");
 console.log(name);
 socket.emit("new-user-joined", name);
 socket.on("user-joined", (name) => {
-  append(`${name} joined the chart`);
+  appendFun(`${name} joined the chart`);
+});
+socket.on(`receive`, (data) => {
+  appendFun(`${data.name}:${data.message}`);
 });
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -7,13 +7,16 @@ const users = {};
 io.on("connection", (socket) => {
   //socket.io instanace which listen for multiple request
   socket.on("new-user-joined", (name) => {
-    console.log("new User", name);
+    // console.log("new User", name);
     console.log("demo");
+    console.log("new-user-join", name);
     users[socket.id] = name;
     socket.broadcast.emit("user-joined", name);
+    console.log("user-joined", name);
     console.log(users);
   });
   socket.on("send", (message) => {
+    console.log(message);
     socket.broadcast.emit("receive", {
       message: message,
       name: users[socket.id],
